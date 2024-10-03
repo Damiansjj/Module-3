b207373ec90bfcdf8e32e63437ced791c46202ce
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2024-10-03 14:23:33 +0200

    M2

--- M2.mjs
+++ M2.mjs
@@ -2,8 +2,9 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 
+let hoogte = parseFloat(await userInput.question("Geef het hoogte in: "));
 
-for(let tellerhoogte=0; tellerhoogte <4; tellerhoogte ++){
+for(let tellerhoogte = 0; tellerhoogte < hoogte; tellerhoogte ++){
 
 for(let breedteller = 0; breedteller < tellerhoogte; breedteller++){
     process.stdout.write("*");
