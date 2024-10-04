d1cf094cbcc84d972df3732a2798dbfbd94c8d7a
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2024-10-04 09:51:16 +0200

    Create H1.mjs

---
+++ H1.mjs
@@ -0,0 +1,41 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+
+let hoogte = parseFloat(await userInput.question("Geef een hoogte: "));
+
+for(let i = 0; i <= hoogte; i++)
+{
+    let rij = " ";
+
+    for(let j = 1; j <= hoogte - i; j++)
+    {
+        rij += " ";
+    }
+
+    for(let k = 1; k <=(2 * i - 1); k++)
+    {
+        rij += "*";
+    }
+
+    console.log(rij);
+
+}
+for(let i = hoogte - 1; i >= 1; i--)
+    {
+        let rij = " ";
+
+        for(let j = 1; j <= hoogte - i; j++)
+        {
+            rij += " ";
+        }
+
+        for(let k = 1; k <=(2 * i- 1); k++)
+        {
+            rij += "*";
+        }
+
+        console.log(rij);
+
+    }
